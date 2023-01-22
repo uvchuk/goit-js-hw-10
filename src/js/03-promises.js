db8c5818230cd71a4171d835f4ae248441c32f79
uvchuk <uvchuk@gmail.com>
2023-01-22 19:26:41 +0100

    end 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
+const refs = {
+  delayIn: document.querySelector('input[name="delay"]'),
+  stepIn: document.querySelector('input[name="step"]'),
+  amountIn: document.querySelector('input[name="amount"]'),
+  createBtn: document.querySelector('button[type="submit"]'),
+};
+
+refs.createBtn.addEventListener('click', onSubmit);
+let position = 0;
+let delayInValue = 0;
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  delayInValue = refs.delayIn.value;
+  refs.createBtn.disabled = true;
+  setTimeout(showPromise, delayInValue);
+}
+
+function showPromise() {
+  const stepInValue = refs.stepIn.value;
+  const amountInValue = refs.amountIn.value;
+  let summaryDelay = +delayInValue;
+  const promise = setInterval(() => {
+    position += 1;
+    if (position <= amountInValue) {
+      createPromise(position, summaryDelay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+        });
+      summaryDelay += +stepInValue;
+    } else {
+      clearInterval(promise);
+      position = 0;
+      refs.createBtn.disabled = false;
+    }
+  }, stepInValue);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      // Fulfill
+      resolve({
+        position,
+        delay,
+      });
+    } else {
+      // Reject
+      reject({
+        position,
+        delay,
+      });
+    }
+  });
 }
